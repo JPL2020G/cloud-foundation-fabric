6c9978efe2aa428a4dd5cf44c0a4fb165f4464e0
JP Laudani <laudani@bjbloemker.joonix.net>
2025-04-21 16:41:52 -0300

    firewall policies folder

--- fast/stages/2-networking-a-simple/net-stg.tf
+++ fast/stages/2-networking-a-simple/net-stg.tf
@@ -14,30 +14,30 @@
  * limitations under the License.
  */
 
-# tfdoc:file:description QA spoke VPC and related resources.
+# tfdoc:file:description Staging spoke VPC and related resources.
 
 locals {
   # streamline VPC configuration conditionals for modules by moving them here
-  qa_cfg = {
-    cloudnat    = var.vpc_configs.qa.cloudnat.enable == true
-    dns_logging = var.vpc_configs.qa.dns.enable_logging == true
-    dns_policy  = var.vpc_configs.qa.dns.create_inbound_policy == true
-    fw_classic  = var.vpc_configs.qa.firewall.use_classic == true
+  stg_cfg = {
+    cloudnat    = var.vpc_configs.stg.cloudnat.enable == true
+    dns_logging = var.vpc_configs.stg.dns.enable_logging == true
+    dns_policy  = var.vpc_configs.stg.dns.create_inbound_policy == true
+    fw_classic  = var.vpc_configs.stg.firewall.use_classic == true
     fw_order = (
-      var.vpc_configs.qa.firewall.policy_has_priority == true
+      var.vpc_configs.stg.firewall.policy_has_priority == true
       ? "BEFORE_CLASSIC_FIREWALL"
       : "AFTER_CLASSIC_FIREWALL"
     )
-    fw_policy = var.vpc_configs.qa.firewall.create_policy == true
+    fw_policy = var.vpc_configs.stg.firewall.create_policy == true
   }
 }
 
-module "qa-spoke-project" {
+module "stg-spoke-project" {
   source          = "../../../modules/project"
   billing_account = var.billing_account.id
-  name            = "gcp-qa-net-prj-spoke-0"
+  name            = "gcp-stg-net-prj-spoke-0"
   parent = coalesce(
-    var.folder_ids.networking-qa,
+    var.folder_ids.networking-stg,
     var.folder_ids.networking
   )
   prefix = var.prefix
@@ -59,16 +59,16 @@ module "qa-spoke-project" {
   metric_scopes = [module.landing-project.project_id]
   # optionally delegate a fixed set of IAM roles to selected principals
   iam = {
-    (var.custom_roles.project_iam_viewer) = try(local.iam_viewer["qa"], [])
+    (var.custom_roles.project_iam_viewer) = try(local.iam_viewer["stg"], [])
   }
   iam_bindings = (
-    lookup(local.iam_admin_delegated, "qa", null) == null ? {} : {
+    lookup(local.iam_admin_delegated, "stg", null) == null ? {} : {
       sa_delegated_grants = {
         role    = "roles/resourcemanager.projectIamAdmin"
-        members = try(local.iam_admin_delegated["qa"], [])
+        members = try(local.iam_admin_delegated["stg"], [])
         condition = {
-          title       = "qa_stage3_sa_delegated_grants"
-          description = "${var.environments["qa"].name} host project delegated grants."
+          title       = "stg_stage3_sa_delegated_grants"
+          description = "${var.environments["stg"].name} host project delegated grants."
           expression = format(
             "api.getAttribute('iam.googleapis.com/modifiedGrantsByRole', []).hasOnly([%s])",
             local.iam_delegated
@@ -77,27 +77,27 @@ module "qa-spoke-project" {
       }
     }
   )
-  # tag_bindings = local.has_env_folders ? {} : {
-  #   environment = local.env_tag_values["qa"]
-  # }
+   tag_bindings = local.has_env_folders ? {} : {
+     environment = local.env_tag_values["stg"]
+   }
 }
 
-module "qa-spoke-vpc" {
+module "stg-spoke-vpc" {
   source                          = "../../../modules/net-vpc"
-  project_id                      = module.qa-spoke-project.project_id
-  name                            = "gcp-qa-net-spoke-0"  
-  mtu                             = var.vpc_configs.qa.mtu
+  project_id                      = module.stg-spoke-project.project_id
+  name                            = "gcp-stg-net-spoke-0"
+  mtu                             = var.vpc_configs.stg.mtu
   delete_default_routes_on_create = true
-  dns_policy = !local.qa_cfg.dns_policy ? {} : {
+  dns_policy = !local.stg_cfg.dns_policy ? {} : {
     inbound = true
-    logging = local.qa_cfg.dns_logging
+    logging = local.stg_cfg.dns_logging
   }
   factories_config = {
     context        = { regions = var.regions }
-    subnets_folder = "${var.factories_config.subnets}/qa"
+    subnets_folder = "${var.factories_config.subnets}/stg"
   }
-  firewall_policy_enforcement_order = local.qa_cfg.fw_order
-  psa_configs                       = var.psa_ranges.qa
+  firewall_policy_enforcement_order = local.stg_cfg.fw_order
+  psa_configs                       = var.psa_ranges.stg
   routes = {
     default = {
       dest_range    = "0.0.0.0/0"
@@ -108,46 +108,46 @@ module "qa-spoke-vpc" {
   }
 }
 
-module "qa-spoke-firewall" {
+module "stg-spoke-firewall" {
   source     = "../../../modules/net-vpc-firewall"
-  count      = local.qa_cfg.fw_classic ? 1 : 0
-  project_id = module.qa-spoke-project.project_id
-  network    = module.qa-spoke-vpc.name
+  count      = local.stg_cfg.fw_classic ? 1 : 0
+  project_id = module.stg-spoke-project.project_id
+  network    = module.stg-spoke-vpc.name
   default_rules_config = {
     disabled = true
   }
   factories_config = {
     cidr_tpl_file = var.factories_config.firewall.cidr_file
-    rules_folder  = "${var.factories_config.firewall.classic_rules}/qa"
+    rules_folder  = "${var.factories_config.firewall.classic_rules}/stg"
   }
 }
 
-module "qa-firewall-policy" {
+module "stg-firewall-policy" {
   source    = "../../../modules/net-firewall-policy"
-  count     = local.qa_cfg.fw_policy ? 1 : 0
-  name      = "qa-spoke-0"
-  parent_id = module.qa-spoke-project.project_id
+  count     = local.stg_cfg.fw_policy ? 1 : 0
+  name      = "stg-spoke-0"
+  parent_id = module.stg-spoke-project.project_id
   region    = "global"
   attachments = {
-    qa-spoke-0 = module.qa-spoke-vpc.id
+    stg-spoke-0 = module.stg-spoke-vpc.id
   }
   factories_config = {
     cidr_file_path          = var.factories_config.firewall.cidr_file
-    egress_rules_file_path  = "${var.factories_config.firewall.policy_rules}/qa/egress.yaml"
-    ingress_rules_file_path = "${var.factories_config.firewall.policy_rules}/qa/ingress.yaml"
+    egress_rules_file_path  = "${var.factories_config.firewall.policy_rules}/stg/egress.yaml"
+    ingress_rules_file_path = "${var.factories_config.firewall.policy_rules}/stg/ingress.yaml"
   }
   security_profile_group_ids = var.security_profile_groups
 }
 
-module "qa-spoke-cloudnat" {
+module "stg-spoke-cloudnat" {
   source = "../../../modules/net-cloudnat"
   for_each = toset(
-    local.qa_cfg.cloudnat ? values(module.qa-spoke-vpc.subnet_regions) : []
+    local.stg_cfg.cloudnat ? values(module.stg-spoke-vpc.subnet_regions) : []
   )
-  project_id     = module.qa-spoke-project.project_id
+  project_id     = module.stg-spoke-project.project_id
   region         = each.value
-  name           = "qa-nat-${local.region_shortnames[each.value]}"
+  name           = "stg-nat-${local.region_shortnames[each.value]}"
   router_create  = true
-  router_network = module.qa-spoke-vpc.name
+  router_network = module.stg-spoke-vpc.name
   logging_filter = "ERRORS_ONLY"
 }
